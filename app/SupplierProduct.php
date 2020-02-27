958ec5064b789295f16ab8ec3402d8a0a91c1e09
Henry Mbugua <hmbugua@resolution.co.ke>
2020-02-27 12:39:27 +0300

    Updating models with fillable field

--- app/SupplierProduct.php
+++ app/SupplierProduct.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SupplierProduct extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['supplier_id', 'product_id'];
 }
