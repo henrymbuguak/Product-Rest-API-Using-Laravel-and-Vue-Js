d16cdccbf3e017b7615ce3b043e6e7644d09017f
Henry Mbugua <hmbugua@resolution.co.ke>
2020-02-27 17:40:02 +0300

    Product controller

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -3,9 +3,44 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Product;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Support\Facades\Validator;
 
-class ProductController extends Controller
+class ProductController extends BaseController
 {
-    //
+    /**
+     * Display a listing of the resource
+     * @return JsonResponse
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return $this->sendResponse($products->toArray(), 'Product retrieved successfully.');
+    }
+    /**
+     * Store new product
+     * @param Request
+     * @return JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name' => 'required',
+            'description' => 'required',
+            'quantity' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $product = Product::create($input);
+        return $this->sendResponse($product->toArray(), 'Product created successfully.');
+
+    }
 }
